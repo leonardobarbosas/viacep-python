a5c04c8f1b7dc42c1d069e6ea60c307f8e6f27bf
Leonardo Barbosa <leoextreme12345678910@gmail.com>
2024-03-05 14:10:37 -0300

    add

---
+++ teste.py
@@ -0,0 +1,32 @@
+import requests
+
+def main():
+    cepInput = input('Digite o cep que você quer verificar? ')
+
+    if len(cepInput) != 8:
+        print('Quantidade de digitos inválidas')
+        exit()
+        
+    request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cepInput))
+
+    addresData = request.json()
+
+    if 'erro' not in addresData:
+        print('CEP: {}'.format(addresData['cep']))
+        print('Logradouro: {}'.format(addresData['logradouro']))
+        print('Bairro: {}'.format(addresData['bairro']))
+        
+        yesInput = input('Você quer ver todas as informações? (y/n) ')
+        
+        if yesInput == 'y': print(addresData)
+        else: exit()
+
+    else:
+        print('{}: CEP inválido.'.format(cepInput))
+    
+    option = input('Você deseja fazer uma nova consulta? (y/n) ')
+    if option == 'y': main()
+    else: exit()
+        
+if __name__ == '__main__':
+    main()
